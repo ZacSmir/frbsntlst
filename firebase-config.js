98f16c83cd0df6ddaf2a37fea775137e29ca8067
ZacSmir <Zach@smirin.us>
2016-10-24 08:38:43 -0700

    Classadoo autocommit

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,5 @@
 var firebaseConfig = {
-  apiKey: "AIzaSyB4n3T-wB2G2UnIrMUU3hC7SqoXhPEb4L0",
+  apiKey: " AIzaSyBTQz6kmAWd-J_10b4036LxO9Gus7eBhFU",
   authDomain: "classadoo-dev.firebaseapp.com",
   databaseURL: "https://classadoo-dev.firebaseio.com"	
 };
